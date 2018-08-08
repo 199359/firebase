7d99d085fe4de5e42390871fb27c86790a440760
Robert Piotrowski <piotrowskirobert7@gmail.com>
2018-08-08 10:08:17 +0200

    button

---
+++ src/UserList/UserList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Default from './Default'
+
+
+class UserList extends React.Component {
+    state = {
+        users: null,
+        isLoadingUsers: false
+    }
+
+    render() {
+        return (
+            <div>
+                <Default />
+            </div>
+        )
+    }
+}
+
+export default UserList
