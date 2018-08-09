468642dfbd89b85da5a8e5b0621f70f3a089b980
Robert Piotrowski <piotrowskirobert7@gmail.com>
2018-08-09 11:06:56 +0200

    config database

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+  
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyAtTRZOGT_NmVhzdE-0D6Frrl-vO1xS7Dc",
+    authDomain: "test-4b9cd.firebaseapp.com",
+    databaseURL: "https://test-4b9cd.firebaseio.com",
+    projectId: "test-4b9cd",
+    storageBucket: "test-4b9cd.appspot.com",
+    messagingSenderId: "311928593199"
+  }
+
+  firebase.initializeApp(config)
+
+  const database = firebase.database()
+
+  export default database
